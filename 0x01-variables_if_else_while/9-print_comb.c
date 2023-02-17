87b940eca7fc587d111d3b48fcafcec26709fae1
josephpraise199 <yasafhalal@gmail.com>
2023-02-17 12:43:04 -0800

    ./9-print_comb | cat -e

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-
+*main - print number 0 to 9 separated with commas
+*
+*Return: 0 (Success)
+*/
 int main(void)
-        {
-	int a;
-
-	for (a = 0; a < 10; a++) 
+{
+	int num;
+	for (num = 48; num < 58; num++)
 	{
-		putchar(a + '0');
-		if (a < 9) 
+		putchar(num);
+		if (num != 57)
 		{
 			putchar(',');
-			putchar(',');
-          	}
+			putchar(' ');
+		}
 	}
-	putchar('\n');
-	return (0);
-        }
+putchar('\n');
+return (0);
+}	
