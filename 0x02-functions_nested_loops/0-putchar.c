2e92ebcbf3dd6fbc9d83c94455f9ce8501a5ffba
josephpraise199 <yasafhalal@gmail.com>
2023-02-21 11:54:11 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * main -Entry point
- * Return: Always 0
- */
+*main - print _putchar
+*
+*Return: 0 (Success)
+*/
 int main(void)
-{       
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+	{
+		char letter[] = {95, 112, 117, 116, 99, 104, 97, 114};
+		int i;
+
+			for (i = 0; i < 8; i++)
+			{
+				_putchar(letter[i]);
+			}
+		_putchar('\n');
+		return (0);
+	}
