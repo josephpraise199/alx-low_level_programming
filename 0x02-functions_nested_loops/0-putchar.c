cacd4f1c07133ad198c23bef19eb99d25d23f9a0
josephpraise199 <yasafhalal@gmail.com>
2023-09-13 13:04:10 -0700

    all my functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - print _putchar
+*
+*Return: 0 (Success)
+*/
+int main(void)
+	{
+		char letter[] = {95, 112, 117, 116, 99, 104, 97, 114};
+		int i;
+
+			for (i = 0; i < 8; i++)
+			{
+				_putchar(letter[i]);
+			}
+		_putchar('\n');
+		return (0);
+	}
