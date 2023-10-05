602772453f83bd4e1a8296b928bb4442b55ac97c
josephpraise199 <yasafhalal@gmail.com>
2023-10-05 13:03:58 -0700

    all m files attempted

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * array_range - Creates an array of integers
+ * @min: Min number
+ * @max: Max integer number
+ * Return: Pointer to the newly created array
+ **/
+int *array_range(int min, int max)
+{
+	int *ptr, i;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * (max - min + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		ptr[i] = min++;
+	}
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
+
