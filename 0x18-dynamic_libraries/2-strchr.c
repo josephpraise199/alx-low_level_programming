48815aa1e476224201bac3b4192dcd65709db5ce
josephpraise199 <yasafhalal@gmail.com>
2023-12-11 09:15:55 -0800

    task 0

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String
+ * @c: Character to search
+ * Return: Pointer to the first occurrence of the character c in the string s
+ **/
+char *_strchr(char *s, char c)
+{
+
+	for (; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+
+	if (*s == c)
+		return (s);
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - Len of a string
+ * @s: String
+ * Return: Length
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String
+ * @needle: Substring
+ * Return: The substring in the string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int letter, needle_size, i;
+	char *pos;
+
+	letter = 0;
+
+	needle_size = _strlen(needle);
+	if (needle_size == 0)
+		return (haystack);
+
+	for (; *haystack != 0; haystack++)
+	{
+		if (*haystack == needle[0])
+		{
+			pos = haystack;
+			for (i = 0; needle[i] != 0 && *haystack != 0; i++, haystack++)
+			{
+				if (*haystack != needle[i])
+				{
+					letter = 0;
+					break;
+				}
+
+				letter++;
+			}
+		}
+	}
+
+	if (letter == needle_size)
+		return (pos);
+
+	return (0);
+}
